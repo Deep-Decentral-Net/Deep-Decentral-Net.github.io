d3876c1bcb8194e385c1992073aeae5723067b7d
Sokol491 <44403484+Sokol491@users.noreply.github.com>
2020-08-01 15:14:30 +0500

    Add files via upload

--- js/function.js
+++ js/function.js
@@ -1,66 +1,18 @@
-function getAllUrlParams(url) {
-
-  // извлекаем строку из URL или объекта window
-  var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
-
-  // объект для хранения параметров
-  var obj = {};
-
-  // если есть строка запроса
-  if (queryString) {
-
-    // данные после знака # будут опущены
-    queryString = queryString.split('#')[0];
-
-    // разделяем параметры
-    var arr = queryString.split('&');
-
-    for (var i=0; i<arr.length; i++) {
-      // разделяем параметр на ключ => значение
-      var a = arr[i].split('=');
-
-      // обработка данных вида: list[]=thing1&list[]=thing2
-      var paramNum = undefined;
-      var paramName = a[0].replace(/\[\d*\]/, function(v) {
-        paramNum = v.slice(1,-1);
-        return '';
-      });
-
-      // передача значения параметра ('true' если значение не задано)
-      var paramValue = typeof(a[1])==='undefined' ? true : a[1];
-
-      // преобразование регистра
-      paramName = paramName.toLowerCase();
-      paramValue = paramValue.toLowerCase();
-
-      // если ключ параметра уже задан
-      if (obj[paramName]) {
-        // преобразуем текущее значение в массив
-        if (typeof obj[paramName] === 'string') {
-          obj[paramName] = [obj[paramName]];
-        }
-        // если не задан индекс...
-        if (typeof paramNum === 'undefined') {
-          // помещаем значение в конец массива
-          obj[paramName].push(paramValue);
-        }
-        // если индекс задан...
-        else {
-          // размещаем элемент по заданному индексу
-          obj[paramName][paramNum] = paramValue;
-        }
-      }
-      // если параметр не задан, делаем это вручную
-      else {
-        obj[paramName] = paramValue;
-      }
+function getUrlVar(){
+    var urlVar = window.location.search; // получаем параметры из урла
+    var arrayVar = []; // массив для хранения переменных
+    var valueAndKey = []; // массив для временного хранения значения и имени переменной
+    var resultArray = []; // массив для хранения переменных
+    arrayVar = (urlVar.substr(1)).split('&'); // разбираем урл на параметры
+    if(arrayVar[0]=="") return false; // если нет переменных в урле
+    for (i = 0; i < arrayVar.length; i ++) { // перебираем все переменные из урла
+        valueAndKey = arrayVar[i].split('='); // пишем в массив имя переменной и ее значение
+        resultArray[valueAndKey[0]] = valueAndKey[1]; // пишем в итоговый массив имя переменной и ее значение
     }
-  }
-
-  return obj;
+    return resultArray; // возвращаем результат
 }
-
-
+var q = getUrlVar();
+h1(q,'center');
 
 var output = document.querySelector('.output');
 var button = document.querySelector('.button');
