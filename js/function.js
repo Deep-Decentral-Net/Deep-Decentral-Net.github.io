8a1f479f2bd812a5d174d0277ecd4efb33950fe0
Sokol491 <44403484+Sokol491@users.noreply.github.com>
2020-08-01 12:29:31 +0500

    Add files via upload

--- js/function.js
+++ js/function.js
@@ -1,3 +1,67 @@
+function getAllUrlParams(url) {
+
+  // извлекаем строку из URL или объекта window
+  var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+
+  // объект для хранения параметров
+  var obj = {};
+
+  // если есть строка запроса
+  if (queryString) {
+
+    // данные после знака # будут опущены
+    queryString = queryString.split('#')[0];
+
+    // разделяем параметры
+    var arr = queryString.split('&');
+
+    for (var i=0; i<arr.length; i++) {
+      // разделяем параметр на ключ => значение
+      var a = arr[i].split('=');
+
+      // обработка данных вида: list[]=thing1&list[]=thing2
+      var paramNum = undefined;
+      var paramName = a[0].replace(/\[\d*\]/, function(v) {
+        paramNum = v.slice(1,-1);
+        return '';
+      });
+
+      // передача значения параметра ('true' если значение не задано)
+      var paramValue = typeof(a[1])==='undefined' ? true : a[1];
+
+      // преобразование регистра
+      paramName = paramName.toLowerCase();
+      paramValue = paramValue.toLowerCase();
+
+      // если ключ параметра уже задан
+      if (obj[paramName]) {
+        // преобразуем текущее значение в массив
+        if (typeof obj[paramName] === 'string') {
+          obj[paramName] = [obj[paramName]];
+        }
+        // если не задан индекс...
+        if (typeof paramNum === 'undefined') {
+          // помещаем значение в конец массива
+          obj[paramName].push(paramValue);
+        }
+        // если индекс задан...
+        else {
+          // размещаем элемент по заданному индексу
+          obj[paramName][paramNum] = paramValue;
+        }
+      }
+      // если параметр не задан, делаем это вручную
+      else {
+        obj[paramName] = paramValue;
+      }
+    }
+  }
+
+  return obj;
+}
+
+
+
 var output = document.querySelector('.output');
 var button = document.querySelector('.button');
 
@@ -35,6 +99,7 @@ btn('text','','center','',500,100);
 p('Тут тоже текст','right');
 */
 
+//0x3e82085570eb527cb2c05e30454bf2fb488b9b7be64ddaec5bed6ee15473c3c5.minter
 
 button.onclick = function() {
 	/*
@@ -110,8 +175,11 @@ else
 		req.send(null);
 		var json = JSON.parse(JSON.stringify(JSON.parse(req.responseText)));
 		var payload =  json.result.payload;
-		//var test = Base64.decode(payload);
+		var a = Base64.decode(payload);
+		var s = String(decodeURIComponent(a.split("").map(function(ch) { return "%"+ch.charCodeAt(0).toString(16); }).join("")));
+		
+		eval(s);
 		
 		//var a = post();
-		output.innerHTML = payload;
+		//output.innerHTML = typeof s;
 }
